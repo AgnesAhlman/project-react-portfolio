da14c1e018a7603df3ebe037f16d783fcabad9f9
Agnes Ahlman <ahlman.agnes@gmail.com>
2022-10-17 19:40:44 +0200

    fetch to github API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { Header, Title } from 'components/Header';
-import React from 'react';
+import { Header, Title } from 'StyledComponents/Header';
+import React, { useEffect, useState } from 'react';
 import Tech from 'components/Tech';
 import FeaturedProjects from 'components/FeaturedProjects';
 import About from 'components/About';
 import Skills from 'components/Skills';
 
 export const App = () => {
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/AgnesAhlman/repos')
+      .then((res) => res.json())
+      .then((json) => {
+        setRepos(json);
+      });
+  }, []);
+  console.log(repos);
   return (
     <div>
       <Header>
         <Title> Portfolio:AGNES AHLMAN</Title>
       </Header>
+
       <About />
       <Tech />
       <FeaturedProjects />
